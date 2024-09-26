6b4028e29ffb0a29f251a2fc5ea39a96fbf5f47d
Gshufelts <gshufelt@stevens.edu>
2024-09-26 15:00:50 -0400

    add triangle code

--- Triangle.py
+++ Triangle.py
@@ -19,10 +19,10 @@ def classifyTriangle(a,b,c):
     
     return:
         If all three sides are equal, return 'Equilateral'
-        If exactly one pair of sides are equal, return 'Isoceles'
+        If exactly one pair of sides are equal, return 'Isosceles'
         If no pair of  sides are equal, return 'Scalene'
         If not a valid triangle, then return 'NotATriangle'
-        If the sum of any two sides equals the squate of the third side, then return 'Right'
+        If the sum of any two sides equals the square of the third side, then return 'Right'
       
       BEWARE: there may be a bug or two in this code
     """
