cdec7e82895d7c1e177dcd0915cb99a6ba7c4f5c
Gshufelts <gshufelt@stevens.edu>
2024-10-01 19:00:14 -0400

    test2

--- TestTriangle.py
+++ TestTriangle.py
@@ -39,7 +39,7 @@ class TestTriangles(unittest.TestCase):
         self.assertEqual(classifyTriangle(9,9,9),'Equilateral', 'All three sides of the triangle are equal meaning it is equilateral')
 
     def testIsRight1(self):
-        self.assertEqual(classifyTriangle(3,4,5),'Right', 'A 3,4,5 triangle is a right triangle')
+        self.assertEqual(classifyTriangle(3,4,5),'Righ', 'A 3,4,5 triangle is a right triangle')
 
     def testIsRight2(self):
         self.assertEqual(classifyTriangle(5,12,13),'Righ', 'A 5,12,13 triangle is a right triangle')
